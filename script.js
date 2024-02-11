32ff47dfdc6ec192b0bf8c0d882136bd1f4c948a
Badal Kumar <112364403+beyond-sky33@users.noreply.github.com>
2024-02-11 23:05:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let start = document.querySelector("#start");
+let stop = document.querySelector("#stop");
+let h3 = document.querySelector("h3");
+
+let interval;
+start.addEventListener("click", function () {
+    let count = 0;
+    interval = setInterval(function () {
+        h3.textContent = count;
+        count++;
+    }, 1000);
+});
+
+stop.addEventListener("click",function(){
+    clearInterval(interval);
+})
